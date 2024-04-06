25195600603b7880be9239e69e03912897337ee6
michael <michaelvu@sirayatech.com>
2024-04-06 13:13:14 +0700

    Section49: Handle message queue TTL

--- server.js
+++ server.js
@@ -2,11 +2,29 @@
 
 const messageService = require("./src/services/consumerQueue.service");
 const queueName = "test-topic";
+// messageService
+//   .consumerQueue(queueName)
+//   .then(() => {
+//     console.log(`Consumer queue ${queueName} started success`);
+//   })
+//   .catch((error) => {
+//     console.error(`Consumer queue ${queueName} fail`, error);
+//   });
+
 messageService
-  .consumerQueue(queueName)
+  .consumerToQueueNormal()
   .then(() => {
-    console.log(`Consumer queue ${queueName} started success`);
+    console.log(`consumerToQueueNormal queue started success`);
   })
   .catch((error) => {
-    console.error(`Consumer queue ${queueName} fail`, error);
+    console.error(`consumerToQueueNormal queue fail`, error);
+  });
+
+messageService
+  .consumerToQueueFail()
+  .then(() => {
+    console.log(`consumerToQueueFail queue started success`);
+  })
+  .catch((error) => {
+    console.error(`consumerToQueueFail queue fail`, error);
   });
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -2,6 +2,11 @@
 
 const { connectToRabbitMQ, consumerQueue } = require("../dbs/init.rabbitmq");
 
+const log = console.log;
+console.log = function () {
+  log.apply(console, [new Date()].concat(arguments));
+};
+
 const messageService = {
   consumerQueue: async (queueName) => {
     try {
@@ -9,6 +14,61 @@ const messageService = {
       await consumerQueue(channel, queueName);
     } catch (error) {
       console.error(`Error consumerQueue :::`, error);
+    }
+  },
+  consumerToQueueNormal: async () => {
+    try {
+      const { channel } = await connectToRabbitMQ();
+      const notificationQueue = "notificationQueueProcess";
+
+      const timeoutEx = 5000;
+      setTimeout(() => {
+        channel.consume(notificationQueue, (msg) => {
+          console.log(
+            `consumerToQueueNormal Received message: ${msg.content.toString()}`
+          );
+          channel.ack(msg);
+        });
+      }, 15000);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  consumerToQueueFail: async () => {
+    try {
+      const { channel } = await connectToRabbitMQ();
+      const notificationQueueHandler = "notificationQueueHotfix";
+      const notificationExchangeDLX = "notificationExDLX";
+      const notificationRoutingKeyDLX = "notificationRoutingKeyDLX";
+
+      await channel.assertExchange(notificationExchangeDLX, "direct", {
+        durable: true,
+      });
+
+      const resultQueue = await channel.assertQueue(notificationQueueHandler, {
+        exclusive: false,
+      });
+
+      await channel.bindQueue(
+        resultQueue.queue,
+        notificationExchangeDLX,
+        notificationRoutingKeyDLX
+      );
+
+      await channel.consume(
+        resultQueue.queue,
+        (msgFailed) => {
+          console.log(
+            `consumerToQueueFail error message: ${msgFailed.content.toString()}, please hot fix`
+          );
+        },
+        {
+          noAck: true,
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      throw error;
     }
   },
 };
